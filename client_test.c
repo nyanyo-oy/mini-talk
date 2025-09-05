2f2d3b53a67d7aa4616c65f5c04b1d7264898175
nyanyo-oy <68849499+nyanyo-oy@users.noreply.github.com>
2025-09-05 11:33:46 +0900

    150000 30usleep

--- client_test.c
+++
@@ -1,7 +0,0 @@
-#include <libc.h>
-int main (int arc, char **arv)
-{
-	(void)arc;
-	kill((pid_t)atoi(arv[1]), SIGSEGV);
-	return (0);
-}
--- main.c
+++ main.c
@@ -6,10 +6,32 @@
 #define SUCSSES 1
 #define FAILURE 0
 
-void signal_handler(int signum)
+volatile sig_atomic_t g_task_end = 0;
+
+static void	setup_sigactoion(struct sigaction *sa, void (*handler)(int, siginfo_t *, void *))
+{
+	sa->sa_sigaction = handler;
+	sa->sa_flags = SA_SIGINFO;
+	sigemptyset(&sa->sa_mask);
+}
+
+void	client_task_end_handler(int signum, siginfo_t *info, void *context)
+{
+	(void)info;
+	(void)context;
+	if (signum == SIGUSR1)
+	{
+		g_task_end = 1;
+	}
+}
+
+void signal_handler(int signum, siginfo_t *info, void *context)
 {
 	static unsigned char c;
 	static int	bit_counter = 0;
+	
+	(void)context;
+	// client_pid = info->si_pid;
 
 	if (signum == SIGUSR1)
 	{
@@ -26,7 +48,9 @@ void signal_handler(int signum)
 		bit_counter = 0;
 		c = 0;
 	}
+	kill(info->si_pid, SIGUSR1);
 }	
+
 
 int server()
 {
@@ -35,47 +59,90 @@ int server()
 	pid =  getpid();
 	printf("Server PID: %d\n", pid);
 	
-	sa.sa_handler = signal_handler;
-	sa.sa_flags = 0;
+	sa.sa_sigaction = signal_handler;
+	sa.sa_flags = SA_SIGINFO;
 	sigemptyset(&sa.sa_mask);
 	sigaction(SIGUSR1, &sa, NULL); //bit0
 	sigaction(SIGUSR2, &sa, NULL); //bit1
-	while(1){
+	
+	while(1)
+	{
 		pause();
-		// 何かでcからの情報を待ち受けて→うけとったら
-		// 情報を加工してコンソールに表示
 	}	
+	return (0);
+}
+
+// if (s_len <= 150000)
+// 	dynamic_delay = 30
+// else 
+// 	dynamic_delay = 30 * (s_len / 150000)
+	
+
+
+static int send_char_bits(pid_t pid, unsigned char c)
+{
+	size_t	n;
+	int	timeout_counter;
+	
+	n = 8;
+	while (n--)
+	{
+		g_task_end = 0;
+		if (!(c & 1))
+		{
+			if (kill(pid, SIGUSR1) == -1)
+			{
+				write(STDERR_FILENO, "Error: Failed to send signal\n", 30);
+				exit(1);
+			}
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) == -1)
+			{
+				write(STDERR_FILENO, "Error: Failed to send signal\n", 30);
+				exit(1);
+			}
+		}
+		
+		timeout_counter = 0;
+		while (!g_task_end && timeout_counter < 5000)
+		{
+			usleep(10);
+			timeout_counter++;
+		}
+		
+		if (!g_task_end)
+		{
+			write(STDOUT_FILENO,"Error: Server response timeout\n", 32);
+                exit(1); 
+		}
+		c = c >> 1;
+		usleep(30);//重要 15万30
+	}
 	return (0);
 }
 
 int	client(pid_t pid, char *str)
 {
 	size_t	i;
-	size_t	n;
-	unsigned char	c;
+	int	s_len;
+	struct sigaction sa;
 	
+	setup_sigactoion(&sa, client_task_end_handler);
+	sigaction(SIGUSR1, &sa, NULL);
+	s_len = ft_strlen(str);
 	i = 0;
 	while (str[i])
 	{
-		c = str[i];
-		n = 8;
-		while (n--)
-		{
-			if (!(c & 1))
-			{
-				if (kill(pid, SIGUSR1) == -1)
-					
-			}
-			else
-			{
-				kill(pid, SIGUSR2);
-			}
-			c = c >> 1;
-			usleep(100);
-		}
+		if (send_char_bits(pid, str[i]) == -1)
+			exit(1);
 		i++;
 	}
-	// kill(pid, SIGUSR3);
+	
+	if (send_char_bits(pid, '\n') == -1)
+		exit (1);
+	
 	return (0);
 }
 
